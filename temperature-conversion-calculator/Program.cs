10277431ca48f425bab001d94e46321a1af0a123
RossaMania <raclettenberg@gmail.com>
2024-05-08 11:18:41 -0500

    feat: Set up namespaces. Add temperature conversion logic. Hard code variables. Set up output.

--- temperature-conversion-calculator/Program.cs
+++ temperature-conversion-calculator/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace temperature_conversion_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          int fahrenheit = 94;;
+          decimal firstCalculation = (decimal)fahrenheit - 32;
+          decimal celsius = firstCalculation * ((decimal)5m / (decimal)9m);
+
+          Console.WriteLine("The temperature is " + celsius + " degrees Celsius.");
+
+        }
+    }
+}
